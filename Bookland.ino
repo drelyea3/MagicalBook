f020ca85ef131a93a5fcc5cf5f026db339b9d427
drelyea3 <relyea_d@msn.com>
2019-04-20 16:40:19 -0700

    works once

--- Bookland.ino
+++ Bookland.ino
@@ -26,9 +26,6 @@ Context g_context;
 
 
 Button pushButton(ACTIVATE_BUTTON_PIN);
-Button openSwitch(OPEN_SWITCH_PIN);
-AnalogReader reader(BRIGHTNESS_PIN, BRIGHTNESS_TOLERANCE);
-Watchdog watchdog(WATCHDOG_TIMEOUT_SECONDS * 1000);
 
 Action* actions[] = {
   new ExtrapolateAction(BLACK, YELLOW, 1000),
@@ -51,26 +48,19 @@ Action* actions[] = {
   new TerminateAction(),
 };
 
-int actionIndex = -2;
+int actionIndex = -1;
 bool actionFinished = false;
 Action* pAction;
 bool isSetupMode = false;
 RGB color;
-byte brightness = 64;
+byte brightness = 255;
 bool isClosed = true;
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting");
 
-  pinMode(POWER_LED_PIN, OUTPUT);
-  analogWrite(POWER_LED_PIN, POWER_LED_BRIGHTNESS);
-
-  pushButton.SetWatchdog(&watchdog);
-
-  reader.CheckState(g_context);
   pushButton.CheckState(g_context);
-  openSwitch.CheckState(g_context);
 
   isSetupMode = pushButton.GetUndebouncedValue() == 1;
 
@@ -89,75 +79,26 @@ void loop()
   g_context.now = millis();
 
   auto pushButtonChanged = pushButton.CheckState(g_context);
-  auto brightnessChanged = reader.CheckState(g_context);
-  auto openChanged = openSwitch.CheckState(g_context);
-  
-  if (openChanged) {
-    isClosed = openSwitch.GetValue() == 0;
-    Serial.print("isClosed A = "); Serial.println(isClosed);
-    if (isClosed)
-    {
-      actionIndex = -1;
-      actionFinished = false;
-      Action::setAll(g_context, BLACK);
-      g_context.strip.show();
-      isClosed = true;
-    }
-  }
 
-  if (isClosed)
+  if (actionIndex == -1 || actionFinished)
   {
-    return;
-  }
-  /*
-  if (watchdog.IsTimeout(g_context))
-  {
-    Action::setAll(g_context, BLACK);
-    actionIndex = -1;
-    actionFinished = false;
-    delay(1000);
-  }
-  else */
-  if (isSetupMode)
-  {
-    if (pushButton.IsPressed())
-    {
-      isSetupMode = false;
-    }
-    Action::setAll(g_context, WHITE);
-  }
-  else // normal action processing
-  {
-    if (actionIndex == -1 || actionFinished)
-    {
-      actionIndex = (actionIndex + 1) % (sizeof(actions) / sizeof(Action*));
+    actionIndex = (actionIndex + 1) % (sizeof(actions) / sizeof(Action*));
 #if TRACE
-      Serial.print("Action "); Serial.println(actionIndex);
+    Serial.print("Action "); Serial.println(actionIndex);
 #endif
-      pAction = actions[actionIndex];
-      actionFinished = false;
-      pAction->Setup(g_context);
-    }
-
-    if (pAction)
-    {
-      actionFinished = !pAction->Step(g_context);
-    }
-
-    if (actionFinished)
-    {
-      pAction->Teardown(g_context);
-    }
+    pAction = actions[actionIndex];
+    actionFinished = false;
+    pAction->Setup(g_context);
   }
 
-  // always respond to brightness changes
-  if (brightnessChanged)
+  if (pAction)
   {
-    //brightness = 255 - reader.GetValue() / 4;
-    //Serial.print("Brightness "); Serial.println(brightness);
-    g_context.strip.setBrightness(brightness);
-    Action::setAll(g_context, g_context.lastColor.color);
-    watchdog.Pat(g_context);
+    actionFinished = !pAction->Step(g_context);
+  }
+
+  if (actionFinished)
+  {
+    pAction->Teardown(g_context);
   }
 
   if (g_context.showNeeded)
